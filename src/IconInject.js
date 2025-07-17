97fe3c004171c8d319daebf0433a4c888328e154
Ayubass <2670833123@qq.com>
2025-07-17 23:04:42 +0800

    成功添加相同样式的图标

--- src/IconInject.js
+++ src/IconInject.js
@@ -1,50 +1,49 @@
 function addMyFuncBarIcon() {
     if (document.querySelector('#id-my-func-bar-icon')) return;
 
-    const newBar = document.querySelector('.sticker-panel__bar.vue-component');
-    if (!newBar) return;
+    // 这里的stickerBar是点击表情后弹出的页面的bar
+    const stickerBar = document.querySelector('.tabs-container');
+    if (!stickerBar) return;
 
-    const barIconElement = document.querySelector('.bar-icon')?.cloneNode(true);
-    if (!barIconElement) return;
+    // 创建新的图标元素
+    const newIconContainer = document.createElement('div');
+    newIconContainer.className = 'tabs-container-item';
+    newIconContainer.id = 'id-my-func-bar-icon'; // 添加唯一ID
+    newIconContainer.setAttribute('data-active-packageid', '4');
+    newIconContainer.setAttribute('data-v-1233ffbd', ''); // 为了让属性与旁边的元素保持一致
 
-    const iconItem = barIconElement.querySelector('.icon-item');
-    const imageElement = barIconElement.querySelector('.q-svg-icon');
-    const tooltip = barIconElement.querySelector('.q-tooltips');
+    const newIcon = document.createElement('i');
+    newIcon.className = 'q-svg-icon q-icon vue-component';
+    newIcon.title = '本地表情包';
+    newIcon.style.cssText = 'width: 24px; height: 24px; --340fd034: var(--icon_primary);';
 
-    iconItem.id = "id-my-func-bar-icon";
-    iconItem.ariaLabel = "我的功能";
+    const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svgElement.setAttribute('viewBox', '0 -960 960 960');
+    svgElement.innerHTML = `<path d="M680-160v-640q33 0 56.5 23.5T760-720v480q0 33-23.5 56.5T680-160ZM160-80q-33 0-56.5-23.5T80-160v-640q0-33 23.5-56.5T160-880h360q33 0 56.5 23.5T600-800v640q0 33-23.5 56.5T520-80H160Zm680-160v-480q25 0 42.5 17.5T900-660v360q0 25-17.5 42.5T840-240Zm-680 80h360v-640H160v640Zm0-640v640-640Z"/>`;
 
-    imageElement.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f">
-        <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
-      </svg>`;
+    newIcon.appendChild(svgElement);
+    newIconContainer.appendChild(newIcon);
 
-    const tip = document.createElement('div');
-    tip.className = 'q-tooltips__content q-tooltips__bottom q-tooltips-div';
-    tip.style.cssText = 'bottom: -31px; transform: translateX(-50%); left: calc(50% + 0px);';
-    const inner = document.createElement('div');
-    inner.className = 'primary-content';
-    inner.textContent = '这是我的按钮';
-    tip.appendChild(inner);
-    tooltip.appendChild(tip);
+    // 插入到第一个 .tabs-container-item 前
+    const firstItem = stickerBar.querySelector('.tabs-container-item');
+    if (firstItem) {
+        stickerBar.insertBefore(newIconContainer, firstItem);
+    }
 
-    iconItem.addEventListener('click', () => {
+    // 添加点击事件
+    newIconContainer.addEventListener('click', () => {
         alert("你点击了我！");
     });
-
-    newBar.insertBefore(barIconElement, newBar.firstChild); // ⬅️ 改为添加到 sticker-panel__bar
 }
-
 
 function watchAndInjectIcon() {
     const observer = new MutationObserver(() => {
-        const newBar = document.querySelector('.sticker-panel__bar.vue-component');
-        if (newBar && !document.querySelector('#id-my-func-bar-icon')) {
+        const stickerBar = document.querySelector('.tabs-container');
+        if (stickerBar && !document.querySelector('#id-my-func-bar-icon')) {
             addMyFuncBarIcon();
         }
     });
     observer.observe(document.body, { childList: true, subtree: true });
 }
-
 
 watchAndInjectIcon();
